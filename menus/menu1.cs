6801379982a2193cf03306fd9483581e905f85c3
IsaacRogovski <isaac.rogovski@gmail.com>
2026-04-16 22:54:46 -0300

    menu1 agora é instanciado em programs

--- menus/menu1.cs
+++ menus/menu1.cs
@@ -8,11 +8,8 @@ namespace menuBasicoCsharp.menus
         public static void criarMenu1()
         {
             Funcoes funcoes = new Funcoes();
-
-
-
             bool sistema = true;
-            int opcao;
+            string? opcao;
 
             while (sistema)
             {
@@ -27,51 +24,48 @@ namespace menuBasicoCsharp.menus
                 Console.WriteLine("├ 7 - Calcular média de DUAS notas.");
                 Console.WriteLine("├ 8 - Ler 2 numeros, exibi-los e depois troca-los de lugar.");
                 Console.WriteLine("├ 9 - Formula de Baskhara.");
+                Console.WriteLine("├ E - Proxima pagina.");
                 Console.WriteLine("└ 0 - SAIR.");
                 Console.Write("\nInsira a opção que deseja: ");
-                if (int.TryParse(Console.ReadLine(), out opcao))
+                opcao = Console.ReadLine().ToLower();
+
+                switch (opcao)
                 {
-                    switch (opcao)
-                    {
-                        case 1:
-                            funcoes.verificarMaior();
-                            break;
-                        case 2:
-                            funcoes.votar();
-                            break;
-                        case 3:
-                            funcoes.ePar();
-                            break;
-                        case 4:
-                            funcoes.multiploDe5();
-                            break;
-                        case 5:
-                            funcoes.maiorOuIgual10();
-                            break;
-                        case 6:
-                            funcoes.valorItens();
-                            break;
-                        case 7:
-                            funcoes.mediaNota();
-                            break;
-                        case 8:
-                            funcoes.trocarValores();
-                            break;
-                        case 9:
-                            funcoes.baskara();
-                            break;
-                        case 10:
-                            PodeDirigir.verificarSePodeDirigir();
-                            break;
-                        case 11:
-                            DefinirIdade.definirSeAdultoJovemMenorDeIdade();
-                            break;
-                        case 0:
-                            sistema = false;
-                            break;
-                        default:
-                            break;
-                    }
+                    case "1":
+                        funcoes.verificarMaior();
+                        break;
+                    case "2":
+                        funcoes.votar();
+                        break;
+                    case "3":
+                        funcoes.ePar();
+                        break;
+                    case "4":
+                        funcoes.multiploDe5();
+                        break;
+                    case "5":
+                        funcoes.maiorOuIgual10();
+                        break;
+                    case "6":
+                        funcoes.valorItens();
+                        break;
+                    case "7":
+                        funcoes.mediaNota();
+                        break;
+                    case "8":
+                        funcoes.trocarValores();
+                        break;
+                    case "9":
+                        funcoes.baskara();
+                        break;
+                    case "e":
+                        
+                        break;
+                    case "0":
+                        sistema = false;
+                        break;
+                    default:
+                        break;
                 }
             }
         }
